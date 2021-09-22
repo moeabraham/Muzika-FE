0ffcacf64cff48807121a0278a3f1febde461413
Your Name <=>
2021-09-21 20:08:32 -0500

    test

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 
 export default function App() {
   const [state, setState] = useState({
-    tracks: [{ name: "JavaScript", album: 4 }],
+    tracks: [{ track:"", album:""}],
     newTrack:{
-      name:"",
-      album:"3"
+      track:"",
+      album:"",
+      year:"",
+      url:"",
     }
   });
 
@@ -14,6 +16,7 @@ export default function App() {
 
     // make AJAX request (property shorthand props)
     async function getAppData(){
+      
       const tracks = await fetch('http://localhost:3001/api/tracks')
       .then(res => res.json())
 
@@ -27,15 +30,41 @@ export default function App() {
     // put incoming data into state(aka update state)
   },[]);
 
-  function addTrack(e) {
+   async function handleSubmit(e) {
     e.preventDefault();
-    setState({
-      tracks: [...state.tracks, state.newTrack],
-      newTrack: {
-        name: "",
-        album: "3"
-      }
-    })
+
+    try {
+      const track = await fetch('http://localhost:3001/api/tracks', {
+        method:'POST',
+        headers: {
+          'Content-type' : 'Application/json'
+        },
+        body: JSON.stringify(state.newTrack)
+      }).then(res => res.json())
+      
+        
+        setState({
+          tracks: [...state.tracks, track],
+          newTrack: {
+            track: "",
+            album: "",
+            year:"",
+            url:""
+       
+          }
+        })
+        
+  
+
+
+    } catch(error) {
+      console.log(error)
+    }
+
+
+
+
+
   }
 // when updating the form we are merging old values with new values while with add skill we are just replacing the latest data
   function handleChange(e){
@@ -55,20 +84,25 @@ export default function App() {
       <hr />
       {state.tracks.map((s, i) => (
         <article key={i}>
-          <div>{s.name}</div> <div>{s.album}</div>
+          <div>{s.track}</div> <div>{s.album}</div>
         </article>
       ))}
       <hr />
-      <form onSubmit={addTrack}>
+      <form onSubmit={handleSubmit}>
         <label>
-          <span>name</span>
-          <input name="name" value={state.newTrack.name} onChange={handleChange}  />
+          <span>track name</span>
+          <input name="track" value={state.newTrack.track} onChange={handleChange}  />
         </label>
         <label>
           <span>album</span> 
-          <input name="album" value={state.newTrack.album} onChange={handleChange}  />
 
-          {/* <select name="album" value={state.newTrack.album} onChange={handleChange}  >
+          <input name="album" value={state.newTrack.album} onChange={handleChange}  />
+          <input name="year" value={state.newTrack.year} onChange={handleChange}  />
+          <input name="url" value={state.newTrack.url} onChange={handleChange}  />
+
+
+
+          {/* <select name="year" value={state.newTrack.year} onChange={handleChange}  >
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
