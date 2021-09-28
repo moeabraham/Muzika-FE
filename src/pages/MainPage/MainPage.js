c898d045749880ac67f4b1dba9ced837a97b93d5
Your Name <=>
2021-09-28 15:48:28 -0500

    test

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,12 +1,14 @@
 
 import Header from '../../components/Header/Header';
+import {Link} from 'react-router-dom'
 
 const MainPage = (props) => {
 
     return (
         <>
         <Header user={props.userState.user} />
-        
+        <Link className= "btn btn-default" to='/formpage'> Home</Link>
+
         <h1>Main Page </h1>
         
         </>
